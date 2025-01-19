832d821f79d5b014c000cc230f1e07bddc566476
Lucas Moreira <lucasmoreira2005@hotmail.com>
2025-01-19 12:38:40 -0300

    feat:add CEP service

---
+++ src/main/java/com/example/apicep/service/CepService.java
@@ -0,0 +1,34 @@
+package com.example.apicep.service;
+
+import com.example.apicep.dto.CEP;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class CepService {
+
+
+    public CEP getCEP(String cep){
+
+        try {
+            String URI = "https://viacep.com.br/ws/" + cep + "/json/";
+            String URL = "/api";
+
+            CEP response = WebClient
+                    .create(URL)
+                    .get()
+                    .uri(URI, cep)
+                    .retrieve()
+                    .bodyToMono(CEP.class).block();
+
+            return response;
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+}
